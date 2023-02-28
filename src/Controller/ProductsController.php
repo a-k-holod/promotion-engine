2a28a5c320cb9694d03e717db55192ea5e33cc6c
Angelika <angelika.holod@gmail.com>
2023-02-28 23:32:54 +0100

    Response but with camel case instead of snake case.

--- src/Controller/ProductsController.php
+++ src/Controller/ProductsController.php
@@ -2,7 +2,48 @@
 
 namespace App\Controller;
 
-class ProductsController
+use App\DTO\LowestPriceEnquiry;
+use PHPUnit\Util\Json;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Serializer\SerializerInterface;
+
+class ProductsController extends AbstractController
 {
 
+  #[Route('/products/{id}/lowest-price', name: 'lowest-price', methods: 'POST')]
+  public function lowestPractice(int $id, Request $request, SerializerInterface $serializer): Response
+  {
+
+    if ($request->headers->has('force_fail')) {
+      return new JsonResponse([
+        'error' => "Promotion Engine fail error msg!",
+      ],
+      $request->headers->get('force_fail')
+      );
+    }
+
+    /** @var LowestPriceEnquiry $lowestPriceEnquiry */
+    $lowestPriceEnquiry = $serializer->deserialize($request->getContent(), LowestPriceEnquiry::class, 'json');
+
+
+    $lowestPriceEnquiry->setDiscountedPrice(50);
+    $lowestPriceEnquiry->setPrice(110);
+    $lowestPriceEnquiry->setPromotionId(2);
+    $lowestPriceEnquiry->setPromotionName("Black Friday half price promo");
+
+
+    return new JsonResponse($lowestPriceEnquiry, 200);
+
+  }
+
+
+  #[Route('/products/{id}/promotions', name: 'promotions', methods: 'GET')]
+  public function promotions()
+  {
+
+  }
 }
