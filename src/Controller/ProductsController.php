824c3d6aae7fe91fab753eee13ebb886442cb0f9
Angelika <angelika.holod@gmail.com>
2023-03-01 11:40:56 +0100

    Serializer added. Return response is in format initially passed - from camelCase to snake_case.

--- src/Controller/ProductsController.php
+++ src/Controller/ProductsController.php
@@ -3,19 +3,19 @@
 namespace App\Controller;
 
 use App\DTO\LowestPriceEnquiry;
+use App\Service\Serializer\DTOSerializer;
 use PHPUnit\Util\Json;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\Serializer\SerializerInterface;
 
 class ProductsController extends AbstractController
 {
 
   #[Route('/products/{id}/lowest-price', name: 'lowest-price', methods: 'POST')]
-  public function lowestPractice(int $id, Request $request, SerializerInterface $serializer): Response
+  public function lowestPractice(int $id, Request $request, DTOSerializer $serializer): Response
   {
 
     if ($request->headers->has('force_fail')) {
@@ -35,11 +35,10 @@ class ProductsController extends AbstractController
     $lowestPriceEnquiry->setPromotionId(2);
     $lowestPriceEnquiry->setPromotionName("Black Friday half price promo");
 
-
-    return new JsonResponse($lowestPriceEnquiry, 200);
+    $responseContent = $serializer->serialize($lowestPriceEnquiry, 'json');
+    return new Response($responseContent, 200);
 
   }
-
 
   #[Route('/products/{id}/promotions', name: 'promotions', methods: 'GET')]
   public function promotions()
